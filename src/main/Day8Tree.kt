fb5411ee7c16f8f77e5cd89d00cdb41ed95fbd3b
Jens Penny <el.tutter@gmail.com>
2018-12-08 23:00:42 +0100

    day 8 of advent of code added in kotlin

---
+++ src/main/Day8Tree.kt
@@ -0,0 +1,69 @@
+package main
+
+import jdk.nashorn.api.tree.Tree
+import util.readAsResource
+import java.util.stream.Collectors
+
+fun main(args: Array<String>) {
+    val start = System.currentTimeMillis()
+    val lines = readAsResource("day8")
+    val treeInput = lines.first().split(" ")
+    val root = createTree(treeInput.map { s -> s.toInt() })
+    val sum = calculateMetadataSum(root.first)
+    val stop = System.currentTimeMillis()
+
+    println("Part 1: sum is $sum - found in ${stop - start} ms")
+    val value = calculateValue(root.first)
+    println("Part 2: value is $value")
+}
+
+fun createTree(input: List<Int>): Pair<TreeNode, List<Int>> {
+    val header = input.subList(0, 2)
+    val amountChildren = header[0]
+    val amountMetadata = header[1]
+
+    val node = TreeNode()
+    var restInput = input.drop(2)
+    for (child in 1..amountChildren) {
+        val result = createTree(restInput)
+        restInput = result.second
+        node.children.add(result.first)
+    }
+
+    for (metadata in 1..amountMetadata) {
+        node.metaData.add(restInput[metadata - 1])
+    }
+
+    return Pair(node, restInput.drop(amountMetadata))
+}
+
+fun calculateMetadataSum(node: TreeNode): Int {
+    val total = node.metaData.sum()
+    val childSum = node.children.stream().map { n -> calculateMetadataSum(n) }.collect(Collectors.toList())
+
+    return total + childSum.sum()
+}
+
+fun calculateValue(node: TreeNode) : Int {
+    return node.value()
+}
+
+class TreeNode {
+    val children: MutableList<TreeNode> = arrayListOf()
+    val metaData: MutableList<Int> = arrayListOf()
+
+    fun value(): Int {
+        return if (children.isEmpty()) {
+            metaData.sum()
+        } else {
+            metaData
+                .map { v ->
+                    if (children.size < v) {
+                        0
+                    } else {
+                        children[v - 1].value()
+                    }
+                }.sum()
+        }
+    }
+}
---
+++ test/main/Day8Test.kt
@@ -0,0 +1,24 @@
+package main
+
+import kotlin.test.assertEquals
+
+fun main(args: Array<String>) {
+    val input = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"
+    val treeInput = input.split(" ")
+    val root = createTree(treeInput.map { s -> s.toInt() })
+
+    firstTest(root)
+    secondTest(root.first)
+}
+
+fun firstTest(root: Pair<TreeNode, List<Int>>) {
+
+    val total = calculateMetadataSum(root.first)
+    assertEquals(138, total)
+    assert(root.second.isEmpty())
+}
+
+fun secondTest(root: TreeNode) {
+    val value = calculateValue(root)
+    assertEquals(66, value)
+}
